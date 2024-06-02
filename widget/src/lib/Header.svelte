3c7518bf54b9260d1a04f4a8567d65aaf38e8578
Madison <madisonwmoore@gmail.com>
2024-06-02 15:04:03 -0500

    latest

--- widget/src/lib/Header.svelte
+++ widget/src/lib/Header.svelte
@@ -1,10 +1,47 @@
 <script lang="ts">
   import { IconX, IconMinus } from "@tabler/icons-svelte";
   import { messageStore } from "./Stores/MessageStore";
+  import { slide } from "svelte/transition";
+
 
   messageStore.subscribe((m) => {
     console.log("Moo");
   });
+  let isVideo = false;
+  let videoElement: HTMLVideoElement;
+  let userVideoElement:HTMLVideoElement;
+
+  const constraints = (window.constraints = {
+    audio: false,
+    video: true,
+  });
+
+  function handleSuccess(stream) {
+    const videoTracks = stream.getVideoTracks();
+    console.log("Got stream with constraints:", constraints);
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    window.stream = stream; // make variable available to browser console
+    videoElement.srcObject = stream;
+    userVideoElement.srcObject=stream;
+    console.log(window.stream)
+  }
+
+  const startStream = async () => {
+    isVideo=!isVideo;
+    console.log("Starting")
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      handleSuccess(stream);
+      // e.target.disabled = true;
+    } catch (e) {
+      console.error(e)
+      // handleError(e);
+    }
+  };
+
+  // onMount(()=>{
+  //   startStream()
+  // })
 </script>
 
 <div class="header">
@@ -13,8 +50,18 @@
     <div class="buttons">
       <button><IconMinus /></button>
       <button><IconX /></button>
-    </div>
+    </div>  <button on:click={() => startStream()}>Start</button>
   </div>
+  {#if isVideo}
+    <div transition:slide class="streaming_container">
+    
+      <!-- <div class="video_wrapper"> -->
+        <video class="main_video" bind:this={videoElement} autoplay playsinline></video>
+      <!-- </div> -->
+      
+      <!-- <video class="user_video" bind:this={userVideoElement} autoplay playsinline></video> -->
+    </div>
+  {/if}
 </div>
 
 <style>
@@ -45,6 +92,41 @@
 
   .buttons {
     display: inline;
+  }
+
+  .video_wrapper{
+    overflow: hidden;
+    background-color: transparent;
+    height:200px;
+    width:auto;
+    padding:5px;
+    background-color: inherit;
+    z-index: 20000;
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+
+  }
+
+  .main_video{
+    background-color: transparent;
+    height:200px;
+    width:auto;
+    /* border-radius: 30px;  */
+    overflow: hidden;
+    box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    border-radius: 10px;
+    /* border-radius: 30px; */
+   
+  }
+
+  .user_video{
+    background-color: transparent;
+    border-radius: 30px;
+    height:80px;
+    width:auto;
   }
 
   button {
